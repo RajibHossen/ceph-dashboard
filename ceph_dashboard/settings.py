544f7ecff729759cae61eb823cf824c5006dfd5e
Rajib Hossen <rajibcse2k10@gmail.com>
2017-03-27 16:38:01 +0600

    changes in middleware classes

--- ceph_dashboard/settings.py
+++ ceph_dashboard/settings.py
@@ -47,8 +47,7 @@ ALLOWED_HOSTS = ['192.168.8.79','cephnode1']
 
 # Application definition
 
-#AUTHENTICATION_BACKENDS = [ 'django_auth_ldap.backend.LDAPBackend',
-AUTHENTICATION_BACKENDS = [ 'django.contrib.auth.backends.RemoteUserBackend',
+AUTHENTICATION_BACKENDS = [ 'django_auth_ldap.backend.LDAPBackend',
                             'django.contrib.auth.backends.ModelBackend']
 
 INSTALLED_APPS = [
@@ -70,7 +69,6 @@ MIDDLEWARE_CLASSES = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.RemoteUserMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
